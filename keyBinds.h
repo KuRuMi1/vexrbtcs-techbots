141b724b26750d62b5875ea2bd19221d204d9713
DarkRaZoR^ <38054145+KuRuMi1@users.noreply.github.com>
2019-12-21 23:13:39 -0600

    Added new class member functions to the KeyBind_Handler class

--- keyBinds.h
+++ keyBinds.h
@@ -14,11 +14,9 @@ class KeyBind_Handler
 {
   public:
     static void ButtonL1Pressed();
-    static void ButtonL1Released();
     static void ButtonL2Pressed();
     static void ButtonL2Released();
     static void ButtonR1Pressed();
-    static void ButtonR1Released();
     static void ButtonR2Pressed();
     static void ButtonR2Released();
 };
