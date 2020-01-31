d12a9853ce4477e15a9a4c3f853125938fe6d0f7
Nico591 <59938332+Nico591@users.noreply.github.com>
2020-01-31 09:00:13 -0600

    Create keyBinds.cpp

---
+++ keyBinds.cpp
@@ -0,0 +1,11 @@
+#include "robot-config.h"
+#include "keyBinds.h"
+
+/* void KeyBind_Handler::ButtonL1Pressed() 
+{
+  // Lifts the cube casket}
+
+void KeyBind_Handler::ButtonL1Released()
+{
+
+} */
