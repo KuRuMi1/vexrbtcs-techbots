492133ce0cab28c18fd01950fed1127ee992f40c
Nico591 <59938332+Nico591@users.noreply.github.com>
2020-01-31 09:00:02 -0600

    Create keyBinds.h

---
+++ keyBinds.h
@@ -0,0 +1,17 @@
+#ifndef KEYBINDS_H
+#define KEYBINDS_H
+
+class KeyBind_Handler
+{
+  public:
+    static void ButtonL1Pressed();
+    static void ButtonL1Released();
+    static void ButtonL2Pressed();
+    static void ButtonL2Released();
+    static void ButtonR1Pressed();
+    static void ButtonR1Released();
+    static void ButtonR2Pressed();
+    static void ButtonR2Released();
+};
+
+#endif // KEYBINDS_H
