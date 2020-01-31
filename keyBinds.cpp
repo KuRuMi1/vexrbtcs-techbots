187aa7ef9db196e12268b88686ec40e690730d85
Nico591 <59938332+Nico591@users.noreply.github.com>
2020-01-31 15:31:03 -0600

    Update keyBinds.cpp

--- keyBinds.cpp
+++ keyBinds.cpp
@@ -1,11 +1,40 @@
 #include "robot-config.h"
 #include "keyBinds.h"
 
-/* void KeyBind_Handler::ButtonL1Pressed() 
+void KeyBind_Handler::ButtonL1Pressed()
 {
-  // Lifts the cube casket}
+  // Lift the casket up
+  liftmotor1.rotateFor( vex::directionType::rev, 720, vex::rotationUnits::deg );
+  liftmotor2.rotateFor( vex::directionType::rev, 720, vex::rotationUnits::deg );
+}
 
-void KeyBind_Handler::ButtonL1Released()
+void KeyBind_Handler::ButtonR1Pressed()
 {
+  // Lift the casket down
+  liftmotor1.rotateFor( vex::directionType::fwd, 720, vex::rotationUnits::deg );
+  liftmotor2.rotateFor( vex::directionType::fwd, 720, vex::rotationUnits::deg );
+}
 
-} */
+void KeyBind_Handler::ButtonL2Pressed()
+{
+  // Open the claw
+  clawmotor.rotateFor( vex::directionType::rev, 180, vex::rotationUnits::deg );
+}
+
+void KeyBind_Handler::ButtonR2Pressed()
+{
+  // Close the calw
+  clawmotor.rotateFor( vex::directionType::fwd, 180, vex::rotationUnits::deg );
+}
+
+void KeyBind_Handler::ButtonXPressed()
+{
+  // Rotate claw up
+  clawtiltmotor.rotateFor( vex::directionType::fwd, 720, vex::rotationUnits::deg );
+}
+
+void KeyBind_Handler::ButtonYPressed()
+{
+  // Rotate claw down
+  clawtiltmotor.rotateFor( vex::directionType::rev, 720, vex::rotationUnits::deg );
+}
